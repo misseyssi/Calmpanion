6a027dd5b65fe3c1b7762acf3f9a6de673cadff2
Alma Christel Mina <150186548+misseyssi@users.noreply.github.com>
2023-11-11 13:00:14 +0800

    NewText

--- Assets/MainScreen.cs
+++ Assets/MainScreen.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoodTracker : MonoBehaviour
+public class MainScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("This is a mood tracker");
+        
     }
 
     // Update is called once per frame
